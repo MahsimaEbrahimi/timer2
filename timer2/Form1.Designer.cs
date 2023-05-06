54a289e92ba0f11cd15b5469a12ef44ff6beac3d
unknown <mahsimaeb83@gmail.com>
2023-05-06 15:23:52 +0430

    changes

--- timer2/Form1.Designer.cs
+++ timer2/Form1.Designer.cs
@@ -29,11 +29,37 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.show_btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // show_btn
+            // 
+            this.show_btn.Location = new System.Drawing.Point(273, 26);
+            this.show_btn.Name = "show_btn";
+            this.show_btn.Size = new System.Drawing.Size(85, 58);
+            this.show_btn.TabIndex = 0;
+            this.show_btn.Text = "show";
+            this.show_btn.UseVisualStyleBackColor = true;
+            this.show_btn.Click += new System.EventHandler(this.show_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(383, 319);
+            this.Controls.Add(this.show_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "timer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button show_btn;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- timer2/Form1.cs
+++ timer2/Form1.cs
@@ -12,9 +12,123 @@ namespace timer2
 {
     public partial class Form1 : Form
     {
+        int timeCs = 0, timeSec = 0, timeMin = 0;
+        bool isactive = false;
         public Form1()
         {
             InitializeComponent();
+        }
+        public void make_lable()
+        {
+            int top, left, height, width;
+            top = this.Height / 10;
+            left = this.Width / 10;
+            height = this.Height / 10;
+            width = this.Width / 10;
+
+            make_buttom(top, left, height, width);
+            Label t = new Label();
+            t.Font = new Font(t.Font.FontFamily, 20);
+            t.Top = 2 * top;
+            t.Left = 4 * left;
+            t.Name = "txtdisplay";
+            t.Width = 3 * width;
+            t.Height = 4 * height;
+            this.Controls.Add(t);
+        }
+
+        public void make_buttom(int top, int left, int height, int width)
+        {
+            string[] names_button = { "reset", "start", "stop" };
+            for (int i = 1; i <= 3; i++)
+            {
+                Button b = new Button();
+                b.Font = new Font(b.Font.FontFamily, 10);
+                b.Text = names_button[i - 1];
+                b.Top = 4 * top;
+                b.Height = height;
+                b.Width = 2 * width;
+                b.Left = ((2 * i) * left);
+                this.Controls.Add(b);
+                if (b.Text == "start")
+                {
+                    b.Click += new EventHandler(start_click);
+
+                }
+                if (b.Text == "stop")
+                {
+                    b.Click += new EventHandler(stop_click);
+
+                }
+                if (b.Text == "reset")
+                {
+                    b.Click += new EventHandler(reset_click);
+
+                }
+
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isactive)
+            {
+                timeCs++;
+                if (timeCs >= 100)
+                {
+                    timeSec++;
+                    timeCs = 0;
+                    if (timeSec >= 60)
+                    {
+                        timeMin++;
+                        timeSec = 0;
+                    }
+                }
+            }
+            show();
+        }
+
+        private void resetTime()
+        {
+            timeCs = 0;
+            timeMin = 0;
+            timeSec = 0;
+        }
+        private void start_click(object sender, EventArgs e)
+        {
+            isactive = true;
+
+        }
+        private void stop_click(object sender, EventArgs e)
+        {
+            isactive = false;
+        }
+
+        private void timer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void show_btn_Click(object sender, EventArgs e)
+        {
+            make_lable();
+        }
+
+        private void reset_click(object sender, EventArgs e)
+        {
+            isactive = false;
+            resetTime();
+        }
+        private void show()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name == "txtdisplay")
+                {
+                    c.Text = $"{timeMin}:{timeSec}:{timeCs}";
+                }
+            }
         }
     }
 }
